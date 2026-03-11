74e2a8e9748a66cb92224942fa368114dfaed2f6
Abi Waqas <info.abiwaqas@gmail.com>
2026-03-12 00:35:32 +0500

    every thing is working properly

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth_router
 
 # create all tables in my sql (if not exists)
@@ -11,6 +12,20 @@ app = FastAPI(
     description="fast api authentication"
 )
 
+# CORS configuration
+origins = [
+    # Vite dev server
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # allows these origins
+    allow_credentials=True,
+    allow_methods=["*"],        # allows GET, POST, PUT, DELETE...
+    allow_headers=["*"],        # allows all headers
+)
 
 # include router
 app.include_router(auth_router.router)
